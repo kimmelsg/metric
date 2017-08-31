fc5c13534314d8540a6d2fab19ad98623454e5a3
Zach Silveira <zackify@gmail.com>
2017-08-31 13:02:27 -0400

    setup subscription server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,34 @@
+import gql from 'graphql';
 import express from 'express';
 import schema from './src/schema';
+import http from 'http';
 import bodyParser from 'body-parser';
 import apollo from 'apollo-server-express';
+import subscriptions from 'subscriptions-transport-ws';
 
 var app = express();
 
 app.use('/graphql', bodyParser.json(), apollo.graphqlExpress({ schema }));
+app.use(
+  '/graphiql',
+  apollo.graphiqlExpress({
+    endpointURL: '/graphql',
+    subscriptionsEndpoint: `ws://localhost:8888/subscriptions`,
+  })
+);
 
-app.listen(8888);
+const server = http.createServer(app);
+
+subscriptions.SubscriptionServer.create(
+  {
+    schema,
+    execute: gql.execute,
+    subscribe: gql.subscribe,
+  },
+  {
+    server,
+    path: '/subscriptions',
+  }
+);
+
+server.listen(8888);
