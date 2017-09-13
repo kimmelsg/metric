3efb81e834bf22102b686a10d9fd0ae7b6c2b672
Zach Silveira <zackify@gmail.com>
2017-09-13 14:22:26 -0400

    add env for websocket url

--- web/src/base/index.js
+++ web/src/base/index.js
@@ -4,9 +4,12 @@ import React from 'react';
 import { ApolloClient, ApolloProvider } from 'react-apollo';
 import { SubscriptionClient } from 'subscriptions-transport-ws';
 
-const client = new SubscriptionClient('ws://localhost:8888/subscriptions', {
-  reconnect: true,
-});
+const client = new SubscriptionClient(
+  `${process.env.REACT_APP_URL}/subscriptions`,
+  {
+    reconnect: true,
+  }
+);
 
 const apolloClient = new ApolloClient({
   networkInterface: client,
